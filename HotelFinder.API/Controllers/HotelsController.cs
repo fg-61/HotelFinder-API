472304c4104d4a9d223ddf7fd552d79abb0622d7
Fatih Gören <fatihgoren61@gmail.com>
2022-05-01 08:25:57 +0300

    IActionResult eklendi

--- HotelFinder.API/Controllers/HotelsController.cs
+++ HotelFinder.API/Controllers/HotelsController.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 namespace HotelFinder.API.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
+    [ApiController]     // auto validation control
     public class HotelsController : ControllerBase
     {
         private IHotelService _hotelService;
@@ -16,29 +16,49 @@ namespace HotelFinder.API.Controllers
             _hotelService = hotelService;
         }
         [HttpGet]
-        public List<Hotel> Get()
+        public IActionResult Get()
         {
-            return _hotelService.GetAllHotels();
+            List<Hotel> hotels = _hotelService.GetAllHotels();
+            return Ok(hotels);
         }
         [HttpGet("{id}")]
-        public Hotel GetById(int id)
+        public IActionResult GetById(int id)
         {
-            return _hotelService.GetHotelById(id);
+            var hotel = _hotelService.GetHotelById(id);
+            if (hotel != null)
+            {
+                return Ok(hotel);   // 200 + data
+            }
+            return NotFound();  // 404
         }
         [HttpPost]
-        public Hotel Post([FromBody]Hotel hotel)
+        public IActionResult Post([FromBody]Hotel hotel)
         {
-            return _hotelService.CreateHotel(hotel);
+            var createdHotel = _hotelService.CreateHotel(hotel);
+            return CreatedAtAction("GetById", new { id = createdHotel.Id }, createdHotel);  // 201 + data
         }
         [HttpPut]
-        public Hotel Put([FromBody] Hotel hotel)
+        public IActionResult Put([FromBody]Hotel hotel)
         {
-            return _hotelService.UpdateHotel(hotel);
+            var updatedHotel = _hotelService.GetHotelById(hotel.Id);
+            if(updatedHotel != null)
+            {
+                _hotelService.UpdateHotel(updatedHotel);
+                return Ok(updatedHotel);    // 200 + data
+            }
+            return NotFound();    // 404
         }
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
-            _hotelService.DeleteHotel(id);
+            var deletedHotel = _hotelService.GetHotelById(id);
+            if(deletedHotel != null)
+            {
+                _hotelService.DeleteHotel(deletedHotel.Id);
+                return Ok(deletedHotel);    // 200
+            }
+            
+            return NotFound();  // 404 
         }
 
     }
--- HotelFinder.Entities/Hotel.cs
+++ HotelFinder.Entities/Hotel.cs
@@ -7,9 +7,10 @@ namespace HotelFinder.Entities
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
+        [Required]
         [StringLength(50)]
         public string Name { get; set; }
+        [Required]
         [StringLength(50)]
         public string City { get; set; }
 
